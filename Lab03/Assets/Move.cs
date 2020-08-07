48300a272a2c5f9f22792a47ee83eff3e2316f34
DESKTOP-325D3AA\Pittawat <pittawat_pan@cmu.ac.th>
2020-08-07 16:47:58 +0700

    push Lab03

---
+++ Lab03/Assets/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float _movementStep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-_movementStep , 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(_movementStep , 0, 0);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0 , 0, _movementStep);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0, 0, -_movementStep);
+        }
+    }
+}
